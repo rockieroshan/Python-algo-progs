9c0707b8c9a113098334b42d050fd9bad6162093
Rakesh Roshan Chandrasheker -X (rakeshrc - COGNIZANT TECHNOLOGY SOLUTIONS INDIA PVT LTD at Cisco) <rakeshrc@cisco.com>
2018-07-20 15:43:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+#number div by 7 not div by 5
+#empty_cell = []
+
+'''for i in range(1000, 2200):
+    if(i%7==0) and (i%5!=0):
+      empty_cell.append(str(i))
+print(','.join(empty_cell))'''
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+#factorial of number
+
+'''def factorial(num):
+    if (num == 0) or (num ==1):
+        return 1
+    else:
+        return num * factorial(num -1)
+print(factorial(2))'''
+
+
+
